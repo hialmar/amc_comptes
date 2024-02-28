325f95f2e287e3501f2dbd130c9357f81455f22f
Patrice Torguet <patrice.torguet@irit.fr>
2024-02-28 18:25:51 +0100

    Tentative de filtrage des logs.

--- src/main/java/fr/miage/amc_comptes/AmcComptesApplication.java
+++ src/main/java/fr/miage/amc_comptes/AmcComptesApplication.java
@@ -2,11 +2,17 @@ package fr.miage.amc_comptes;
 
 import fr.miage.amc_comptes.entities.Compte;
 import fr.miage.amc_comptes.repositories.CompteRepository;
+import io.micrometer.observation.ObservationRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.server.observation.ServerRequestObservationContext;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.util.PathMatcher;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -28,5 +34,20 @@ public class AmcComptesApplication implements CommandLineRunner {
 
             compteRepository.save(c);
         }
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    ObservationRegistry observationRegistry() {
+        PathMatcher pathMatcher = new AntPathMatcher("/");
+        ObservationRegistry observationRegistry = ObservationRegistry.create();
+        observationRegistry.observationConfig().observationPredicate((name, context) -> {
+            if(context instanceof ServerRequestObservationContext) {
+                return !pathMatcher.match("/actuator/**", ((ServerRequestObservationContext) context).getCarrier().getRequestURI());
+            } else {
+                return true;
+            }
+        });
+        return observationRegistry;
     }
 }
